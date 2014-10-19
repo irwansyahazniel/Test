22609cc5cdc61c283591855d2e40ad3c319e774c
Irwansyah Hazniel <irwansyahazniel@gmail.com>
2014-10-19 23:01:45 +0700

    Ubah Output String From Other Linux OS

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloWorld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub");
+		System.out.println("Test Git Hub From Other Linux OS");
 	}
 }
